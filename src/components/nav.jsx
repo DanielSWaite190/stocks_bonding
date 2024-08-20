ddc712a95c208aaabe2223d5f61fd11e32460007
DanielSWaite190 <danielwaite190@gmail.com>
2024-08-19 17:59:29 -0700

    Seting up nav

---
+++ src/components/nav.jsx
@@ -0,0 +1,15 @@
+function Nav() {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>Meet our Team</li>
+      <li>Calendar</li>
+      <li>Chat Room</li>
+      <li>Enroll</li>
+      <li>Needs Assessment</li>
+      <li>Thinkific</li>
+  </ul>
+  );
+}
+
+export default Nav;
