e61c05e131632b56c2640b83143e0ea97c40e57e
DanielSWaite190 <danielwaite190@gmail.com>
2024-09-01 15:19:21 -0700

    Inital wow image and text

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -4,19 +4,11 @@ import "./HeroImage.css"
 
 function HeroImage() {
   return (
-    // <div id="section"><div id="back-g"/></div>
-    // <img id="second" src="/media/citty.webp"/>
-    <div>
-      <p>Scroll Up and Down this page to see the parallax scrolling effect.</p>
-      <div class="parallax"></div>
-      <div style={{height:"1000px;", backgroundColor: "red;", fontSize: "36px"}}>
-        Scroll Up and Down this page to see the parallax scrolling effect.
-        This div is just here to enable scrolling.
-        Tip: Try to remove the background-attachment property to remove the scrolling effect.
-        </div>
+    <div id="citty">
+        <img id="wall" src="./media/s_b_wall.webp" alt='Stocks and Bonding logo'/>
+        <div id="white"/>
     </div>
   );
 }
-
 
 export default HeroImage 
