ab8965a1952826bc8533ff178ad2880820446ac6
DanielSWaite190 <danielwaite190@gmail.com>
2024-09-03 15:03:44 -0700

    new home page

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -1,19 +1,15 @@
+import "./heroImage.css"
 import React from 'react';
 
-import "./HeroImage.css"
-
-function HeroImage() {
+function heroImage() {
   return (
     <div id="citty">
-
-      <div id="bid-div">
-
-        <div id="white" class="wow_i" />
-        <img id="wall" class="wow_i" src="./media/s_b_wall.webp" alt='Stocks and Bonding logo'/>
+      <div>
+        <div id="white" />
+        <img id="wall" src="./media/s_b_wall.webp" alt='Stocks and Bonding logo'/>
       </div>
-
     </div>
   );
 }
 
-export default HeroImage 
+export default heroImage 
