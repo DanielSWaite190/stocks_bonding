ae9fe8fef9556c4568f060461922b9e26c2ae02b
DanielSWaite190 <danielwaite190@gmail.com>
2024-09-05 21:22:17 -0700

    frosted mountains

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,39 +6,32 @@ import HeroImage from '../components/heroImage';
 function home () {
   return (
     <React.Fragment>
-      {/* <HeroImage/>
-      <div id="text1"/>
-
-        <h1>THE STOCKS AND BONDING ACADEMY A CORPORATE FINANCIAL WELLNESS SOLUTIO</h1>
-          <h3>Our team of highly professional and very compassionate, licensed financial coaches and advisors, provide engaging virtual workshops, one on one goal setting sessions and UNLIMITED FINANCIAL COACHING appointments. Our ultimate goal is, through education, to increase the financial health of your team.</h3>
-          <h1>72% OF EMPLOYEES ARE FINANCIALLY STRESSED</h1>
-          <h1>Healing your team's financial stress will impact:</h1>
-
-          <ul>
-            <li>Increase Employer Retention: Financially stressed individuals are 2x more likely to look for a new job</li>
-            <li>Improve Productivity:  Financially Stressed employees lose an average of 11.4 hours of productivity a week</li>
-            <li>Reduce Absenteeism: Money struggles often necessitate needing time off to handle.</li>
-            <li>Boost Company Culture and Morale: Providing a service that will add to the overall health and happiness of their family, let's your team members know that they are cared for by their company.</li>
-          </ul>
-
-          <h1>THE STOCKS AND BONDING ACADEMY</h1>
-          <h1>THE STOCKS AND BONDING ACADEMY</h1>
-          <h1>THE STOCKS AND BONDING ACADEMY</h1>
-          <h1>THE STOCKS AND BONDING ACADEMY</h1>
-          <h1>THE STOCKS AND BONDING ACADEMY</h1>
-          <h1>THE STOCKS AND BONDING ACADEMY</h1>
-          <h1>THE STOCKS AND BONDING ACADEMY</h1>
-          <h1>THE STOCKS AND BONDING ACADEMY</h1>
-
-      <img src='../../public/media/mountains.jpg'/> */}
-
-
       <HeroImage/>
-      <img src='./media/mountains.jpg' alt="benefits"/>
-      <h1>text here</h1>
-      <div id="div"/>
+      <div id="body">
+        <img src='./media/mountains.jpg' alt="benefits"/>
+        <div id="text">
+            <h1>THE STOCKS AND BONDING ACADEMY A CORPORATE FINANCIAL WELLNESS SOLUTIO</h1>
+            <h3>Our team of highly professional and very compassionate, licensed financial coaches and advisors, provide engaging virtual workshops, one on one goal setting sessions and UNLIMITED FINANCIAL COACHING appointments. Our ultimate goal is, through education, to increase the financial health of your team.</h3>
+            <h1>72% OF EMPLOYEES ARE FINANCIALLY STRESSED</h1>
+            <h1>Healing your team's financial stress will impact:</h1>
 
+            <ul>
+              <li>Increase Employer Retention: Financially stressed individuals are 2x more likely to look for a new job</li>
+              <li>Improve Productivity:  Financially Stressed employees lose an average of 11.4 hours of productivity a week</li>
+              <li>Reduce Absenteeism: Money struggles often necessitate needing time off to handle.</li>
+              <li>Boost Company Culture and Morale: Providing a service that will add to the overall health and happiness of their family, let's your team members know that they are cared for by their company.</li>
+            </ul>
 
+            <h1>THE STOCKS AND BONDING ACADEMY</h1>
+            <h1>THE STOCKS AND BONDING ACADEMY</h1>
+            <h1>THE STOCKS AND BONDING ACADEMY</h1>
+            <h1>THE STOCKS AND BONDING ACADEMY</h1>
+            <h1>THE STOCKS AND BONDING ACADEMY</h1>
+            <h1>THE STOCKS AND BONDING ACADEMY</h1>
+            <h1>THE STOCKS AND BONDING ACADEMY</h1>
+            <h1>THE STOCKS AND BONDING ACADEMY</h1>
+          </div>
+      </div>
     </React.Fragment>
   );
 }
