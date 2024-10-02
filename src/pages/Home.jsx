75932885521112ea9719b24758bbde92e2e640e7
DanielSWaite190 <danielwaite190@gmail.com>
2024-10-01 21:42:06 -0700

    homepage: seting up first block of text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,27 +10,38 @@ function home () {
       <div id="body">
         <img src='./media/mountains.jpg' alt="benefits"/>
         <div id="text">
-            <h1>THE STOCKS AND BONDING ACADEMY A CORPORATE FINANCIAL WELLNESS SOLUTIO</h1>
-            <h3>Our team of highly professional and very compassionate, licensed financial coaches and advisors, provide engaging virtual workshops, one on one goal setting sessions and UNLIMITED FINANCIAL COACHING appointments. Our ultimate goal is, through education, to increase the financial health of your team.</h3>
-            <h1>72% OF EMPLOYEES ARE FINANCIALLY STRESSED</h1>
-            <h1>Healing your team's financial stress will impact:</h1>
 
-            <ul>
-              <li>Increase Employer Retention: Financially stressed individuals are 2x more likely to look for a new job</li>
-              <li>Improve Productivity:  Financially Stressed employees lose an average of 11.4 hours of productivity a week</li>
-              <li>Reduce Absenteeism: Money struggles often necessitate needing time off to handle.</li>
-              <li>Boost Company Culture and Morale: Providing a service that will add to the overall health and happiness of their family, let's your team members know that they are cared for by their company.</li>
-            </ul>
+        <section id="s_one">
+          <h1>THE STOCKS AND BONDING ACADEMY</h1>
+          <h2>A CORPORATE FINANCIAL WELLNESS SOLUTIO</h2>
+        </section> 
+            
+        <section id="s_two">
+          <p>
+            Our team of highly professional and very compassionate, 
+            licensed financial coaches and advisors, provide engaging virtual workshops, 
+            one on one goal setting sessions and UNLIMITED FINANCIAL COACHING appointments.
+            Our ultimate goal is, through education, to increase the financial health of your team.
+          </p>
 
-            <h1>THE STOCKS AND BONDING ACADEMY</h1>
-            <h1>THE STOCKS AND BONDING ACADEMY</h1>
-            <h1>THE STOCKS AND BONDING ACADEMY</h1>
-            <h1>THE STOCKS AND BONDING ACADEMY</h1>
-            <h1>THE STOCKS AND BONDING ACADEMY</h1>
-            <h1>THE STOCKS AND BONDING ACADEMY</h1>
-            <h1>THE STOCKS AND BONDING ACADEMY</h1>
-            <h1>THE STOCKS AND BONDING ACADEMY</h1>
-          </div>
+          <h2>72% OF EMPLOYEES ARE FINANCIALLY STRESSED</h2>
+
+          <p>Healing your team's financial stress will impact:</p>
+        </section>
+        
+        <section id="s_three">
+          <ul>
+            <li><u>Increase Employer Retention</u>: Financially stressed individuals are 2x more likely to look for a new job</li>
+            <li><u>Improve Productivity</u>:  Financially Stressed employees lose an average of 11.4 hours of productivity a week</li>
+            <li><u>Reduce Absenteeism</u>: Money struggles often necessitate needing time off to handle.</li>
+            <li><u>
+              Boost Company Culture and Morale</u>: Providing a service that will add to the overall health and happiness of their family, 
+              let's your team members know that they are cared for by their company.
+            </li>
+          </ul>
+        </section>
+        
+        </div>
       </div>
     </React.Fragment>
   );
