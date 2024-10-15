db5914b6793cb4317c2c3139c33068654287634d
DanielSWaite190 <danielwaite190@gmail.com>
2024-10-15 16:25:33 -0700

    Adding calendar block to calendar page

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -3,10 +3,81 @@ import "./Calendar.css"
 
 function calendar() {
   return (
-    <div>
-        <h1>Workshop #1 (January 17, 2025) </h1>
-        <h1>Workshop #1 (January 31, 2025) Free </h1>
+  <React.Fragment>
+  <div class="purple_block" id="calendar"><h1>CALENDAR</h1></div>
+  <div id="body">
+    <div id="top" class="dates">
+      <div>
+        <h4>Workshop #1 (January 17, 2025)</h4>
+        <h6>Available Online</h6>
+      </div>
+      <button class="enroll">Enroll</button>
     </div>
+
+    <div id="bottom" class="dates">
+      <div>
+        <h4>Workshop #1 (January 31, 2025)</h4>
+        <h6>Available Online</h6>
+      </div>
+      <p>Free</p>
+      <button class="enroll">Enroll</button>
+    </div>
+
+    <div id="calendar_block">
+      <p style={{marginBottom: "50px"}}>December 29 - January 4</p>
+
+      <div id="days">
+        <div class="day">
+          <p>SUN</p>
+          <p>29</p>
+        </div>
+
+        <div class="day">
+          <p>MON</p>
+          <p>30</p>
+        </div>
+
+        <div class="day">
+          <p>TUE</p>
+          <p>31</p>
+        </div>
+
+        <div class="day">
+          <p>WED</p>
+          <p>1</p>
+        </div>
+
+        <div class="day">
+          <p>THU</p>
+          <p>2</p>
+        </div>
+        <div class="day">
+          <p>FRI</p>
+          <p>3</p>
+        </div>
+        <div class="day">
+          <p>SAT</p>
+          <p>4</p>
+        </div>
+      </div>
+
+      <div id="bottom" class="dates">
+      <div>
+        <h4>Workshop #1 (January 31, 2025)</h4>
+        <h6>Available Online</h6>
+      </div>
+      <p>Free</p>
+      <button class="enroll">Enroll</button>
+    </div>
+
+
+
+    </div>
+
+
+  </div>
+
+  </React.Fragment>
   );
 }
 
