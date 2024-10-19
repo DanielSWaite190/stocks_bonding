c3ad11917e141a9de679f857ba3862114da4bc7c
DanielSWaite190 <danielwaite190@gmail.com>
2024-10-19 14:00:38 -0700

    Enroll page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/home/Home";
 import Team from "./pages/team/Team"
 import Footer from "./components/footer/Footer";
 import Calendar from "./pages/calendar/Calendar"
+import Enroll from "./pages/enroll/Enroll";
 import Chat from "./pages/chat/Chat";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -19,7 +20,7 @@ function App() {
         <Route path='/team' element={<Team/>} />
         <Route path='/calendar' element={<Calendar/>} />
         <Route path='/Chat' element={<Chat/>} />
-        {/* <Route path='/enroll' element={<Enroll/>} /> */}
+        <Route path='/enroll' element={<Enroll/>} />
         {/* <Route path='/assessment' element={<Assessment/>} /> */}
         {/* <Route path='/thinkific' element={<Thinkific/>} /> */}
         {/* <Route path='/login' element={<Login/>} /> */}
