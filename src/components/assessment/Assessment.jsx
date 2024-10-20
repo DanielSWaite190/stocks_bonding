b3a1668dd88b8e30ffa8b95f53772f0db92dd89f
DanielSWaite190 <danielwaite190@gmail.com>
2024-10-20 16:43:18 -0700

    form drop down css logic

--- src/components/assessment/Assessment.jsx
+++ src/components/assessment/Assessment.jsx
@@ -5,42 +5,90 @@ function Assessment(props) {
   return (props.trigger) ? (
     <div className='popup'>
       <div className='popup-inner'>
+        
+        <form id="form">
+          <h1>Please fill out this questionnaire :</h1>
 
-        <h1>Please fill out this questionnaire :</h1>
+          <div id="top-form">
 
-        <div id="top-form">
-          <p>Name, Age, State you live in and status (Single, Married, Divorced, Widowed) :</p>
-          <p>Email</p>
-        </div>
+            <div style={{width: "50%"}}>
+              <p>
+                Name, Age, State you live in and status <br/>
+                (Single, Married, Divorced, Widowed) :
+              </p>
+              {/* <div class="input"/> */}
+              <input type="text" id="fname" name="fname"/>
+            </div>
+            
+          
 
-        <p>How much non-mortgage debt do you have?</p>
-        <div>
+            <div style={{width: "50%",}}>
+              <br/>
+              <p>Email*</p>
+              {/* <div class="input"/> */}
+              <input type="text" id="fname" name="fname" style={{width: "100%"}}/>
+            </div>
 
-        <div class="dropdown">
-        <button class="dropbtn">Dropdown</button>
-        <div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-        </div>
-      </div>
+          </div>
+        
+          <div>
+            <p>How much non-mortgage debt do you have?</p>
+            <input type="text" id="fname" name="fname"/>
+          </div>
 
-      </div>
+          <div>
+            <p>Do you have a clear plan to pay off your debt?</p>
+            <div class="input"/>
+          </div>
 
+          <div>
+            <p>Do you have a written budget that you stick to monthly ?</p>
+            <div class="input"/>
+          </div>
 
-        <p>Do you have a clear plan to pay off your debt?</p>
-        <p>Do you have a written budget that you stick to monthly ?</p>
-        <p>Do you have a 401k that you have left at a previous place of employment ?</p>
-        <p>Do you feel you may have too much in your savings but fear or lack the knowledge to explore
-        alternatives to just saving?</p>
-        <p>If you have children, do you have a plan to pay for their college?</p>
-        <p>Do you feel confused about the different types of life insurance policies available and how life The fact that there was a huge demand for learning about the
-        insurance fits into your overall financial plan? stock market was the most exciting thing. In order to</p>
-        <p>What issue concerns you the most regarding your finances ?</p>
+          <div>
+            <p>Do you have a 401k that you have left at a previous place of employment ?</p>
 
-        <button>Submit</button>
-        {props.children}
-        <button className='close-btn' onClick={() => props.setTrigger(false)}>close</button>
+            <div class="dropdown">
+              <div class="dropdown-content">
+                <p>Yes</p>
+                <p>No</p>
+              </div>
+            </div>
+
+          </div>
+          
+          <div>
+            <p>
+              Do you feel you may have too much in your savings but fear or lack the knowledge to explore
+              alternatives to just saving?
+            </p>
+            <div class="input"/>
+          </div>
+
+          <div>
+            <p>If you have children, do you have a plan to pay for their college?</p>
+            <div class="input"/>
+          </div>
+
+          <div>
+            <p>
+              Do you feel confused about the different types of life insurance policies available and how life The fact that there was a huge demand for learning about the
+              insurance fits into your overall financial plan? stock market was the most exciting thing. In order to
+            </p>
+            <div class="input"/>
+          </div>
+
+          <div>
+            <p>What issue concerns you the most regarding your finances ?</p>
+            <input type="text" id="fname" name="fname"/>
+          </div>
+
+          <button>Submit</button>
+          {props.children}
+
+          <button className='close-btn' onClick={() => props.setTrigger(false)}>close</button>
+        </form>
       </div>
     </div>
   ): "";
